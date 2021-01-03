405ec9c1849bf2976c4152834753bb12119aa21f
Brandon Jones <cerealfordinnerx@gmail.com>
2021-01-03 14:57:23 -0500

    First Commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from logging import exception
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+from twilio.rest import Client
+
+# Your Account SID from twilio.com/console
+account_sid = "AC19687509f815d920ac2201bdffa9b709"
+# Your Auth Token from twilio.com/console
+auth_token  = "a497c8816ff0e59d501fa4892032df6c"
+
+client = Client(account_sid, auth_token)
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://www.publix.com/pd/publix-chicken-tender-sub/BMO-DSB-100011")
+
+
+#find and click choose store
+choose_store = driver.find_element_by_class_name("cta.button")
+choose_store.click()
+
+#enter zip from search page
+zip_search = driver.find_element_by_id("input_ZIPorCity,Stateorstorenumber42")
+zip_search.click()
+zip_search.send_keys("33309")
+zip_search.send_keys(Keys.RETURN)
+
+#assign a store after waiting for element ro present
+try:
+    confirm_store = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "choose-store-button.button.small"))
+    )
+except:
+    driver.quit()
+confirm_store.click()
+
+
+#check of element with the class on-sale is present
+try:
+    on_sale = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "on-sale"))
+    )
+    print("On Sale!")
+except:
+    print("Not on Sale!")
